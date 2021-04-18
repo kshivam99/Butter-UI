0727e79e1c96b6e44324fa9954ab91cee38b0a90
kshivam99 <sawarn101@gmail.com>
2021-04-19 01:39:09 +0530

    alert

--- src/components/Elements/Input.js
+++ src/components/Elements/Input.js
@@ -24,11 +24,9 @@ function Input() {
           align="left"
           src="https://carbon.now.sh/embed?bg=rgba%28255%2C255%2C255%2C1%29&t=seti&wt=none&l=auto&ds=true&dsyoff=0px&dsblur=0px&wc=true&wa=false&pv=30px&ph=59px&ln=false&fl=1&fm=Hack&fs=14px&lh=163%25&si=false&es=4x&wm=false&code=%2520%253Cdiv%2520className%253D%2522formInput%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cinput%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520type%253D%2522text%2522%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520autoComplete%253D%2522off%2522%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520required%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%252F%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Clabel%2520for%253D%2522name%2522%2520className%253D%2522labelName%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cspan%2520className%253D%2522contentName%2522%253EWrite%2520Label%2520Here%253C%252Fspan%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Flabel%253E%250A%2520%253C%252Fdiv%253E"
           style={{
-            width: "75%",
-            height: "24.5rem",
-            left: "0",
-            border: "none",
-            borderRadius: "1rem",
+            width: "690px",
+            height: "400px",
+            border: "0",
             transform: "scale(1)",
             overflow: "hidden",
           }}
