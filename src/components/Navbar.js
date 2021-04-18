b896c608b38d34463529aa134cf7af5d3fbe5f2b
kshivam99 <sawarn101@gmail.com>
2021-04-18 15:20:55 +0530

    refactor

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,28 @@
-import React from 'react';
+import React from "react";
 import Icon from "./butter.png";
-import { FaBars, FaTimes} from 'react-icons/fa';
+import { FaBars, FaTimes } from "react-icons/fa";
 import { useState } from "react";
 
 function Navbar() {
-    const [showDropDownNav, setShowDropDownNav] = useState(false);
+  const [showDropDownNav, setShowDropDownNav] = useState(false);
 
-    function handleMenuIconClick(){
-        setShowDropDownNav(prev=>!prev);
-    }
-    return (
-        <div className="nav">
-            <div className="menu-icon" onClick={()=>handleMenuIconClick()}>     
-                {!showDropDownNav?<FaBars />:<FaTimes />}
-            </div>
-            <img className="nav-icon" src={Icon} alt=""/>
+  function handleMenuIconClick() {
+    setShowDropDownNav((prev) => !prev);
+  }
+  return (
+    <div className="nav">
+      <div className="menu-icon" onClick={() => handleMenuIconClick()}>
+        {!showDropDownNav ? <FaBars /> : <FaTimes />}
+      </div>
+      <img className="nav-icon" src={Icon} alt="" />
 
-            <ul className={!showDropDownNav?"menu":"menu active"}>
-                <li>
-                    Home
-                </li>
-                <li>
-                    Components
-                </li>
-                <li>
-                    Documentation    
-                </li>
-            </ul>
-        </div>
-    )
+      <ul className={!showDropDownNav ? "menu" : "menu active"}>
+        <li>Home</li>
+        <li>Components</li>
+        <li>Documentation</li>
+      </ul>
+    </div>
+  );
 }
 
-export default Navbar
-
+export default Navbar;
